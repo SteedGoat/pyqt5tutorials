5c4e4b8672cb257cabb50862846bc606d53befa4
SteedGoat <qmh17@mails.tsinghua.edu.cn>
2019-08-05 13:20:58 +0800

    add CenterForm

--- CenterForm.py
+++ CenterForm.py
@@ -1,12 +1,12 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QDesktopWidget,QMainWindow,QApplication
 from PyQt5.QtGui import QIcon
 
 
-class FirstMainWin(QMainWindow):
+class CenterForm(QMainWindow):
     def __init__(self,parent=None):
-        super(FirstMainWin,self).__init__(parent)
+        super(CenterForm,self).__init__(parent)
 
         #设置主窗口的标题
         self.setWindowTitle('第一个主窗口应用')
@@ -14,15 +14,20 @@ class FirstMainWin(QMainWindow):
         #设置窗口耳朵尺寸
         self.resize(400,300)
 
-        def center(self):
-            #获取屏幕坐标系
-
+    def center(self):
+        #获取屏幕坐标系
+        screen = QDesktopWidget().screenGeometry()
+        #获取窗口坐标系
+        size = self.geometry()
+        newLeft = (screen.width() - size.width())/2
+        newTop =  (screen.height() - size.height())/2
+        self.move(newLeft,newTop)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    main = FirstMainWin()
+    main = CenterForm()
     main.show()
 
     sys.exit(app.exec_())
